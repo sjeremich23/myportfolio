7b1aad41b7396cf615fd966c94c4d2e285df192c
Shane Jeremich <shane.jeremich@gmail.com>
2021-02-13 19:04:07 -0700

    changed App.js

--- src/App.js
+++ src/App.js
@@ -1,15 +1,8 @@
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <p>
-          Edit <code>src/App.js</code> and save to reload. Testing
-        </p>
-        <a className="App-link" href="https://reactjs.org" target="_blank" rel="noopener noreferrer">
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <h1>Hi</h1>
+    </>
   );
 }
 
